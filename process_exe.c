3d534b11ecef78ae34420c84349673ac9ee8e60e
Justus Mwendwa <justomwesh3@gmail.com>
2023-08-18 06:59:27 -0700

    execve function

---
+++ process_exe.c
@@ -0,0 +1,47 @@
+#include "process_shell.h"
+
+/**
+ * process_exe - Executes commands
+ * @args: arguments
+ * @env: enviornmetal variable
+ * @args: input
+ */
+
+void process_exe(char **args, char **env)
+{
+	char *cmd = NULL, *actual_path = NULL;
+	pid_t id_child;
+	int exeprocess, status;
+
+	if (args)
+	{
+		if (builtin_cmd(args, env) == 1)
+			return;
+		cmd = args[0];
+		/*call path*/
+		actual_path = process_loc(cmd);
+
+		id_child = fork();
+		if (id_child < 0)
+		{
+			perror("Error, Fork Failed");
+			free(cmd);
+			exit(1);
+		}
+		else if (id_child == 0)
+		{
+			exeprocess = execve(actual_path, args, env);
+			if (exeprocess == -1)
+			{
+				perror("Error: Command Failed!");
+				free_args(args);
+				exit(0);
+			}
+		}
+		else
+		{
+			wait(&status);
+		}
+		free(cmd);
+	}
+}
