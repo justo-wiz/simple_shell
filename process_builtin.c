0322ab677d93e9eefdd881c9842aaf2dd8afa035
Justus Mwendwa <justomwesh3@gmail.com>
2023-08-18 07:22:13 -0700

    bultin commands

---
+++ process_builtin.c
@@ -0,0 +1,54 @@
+#include "process_shell.h"
+/**
+ * builtin_cmd - Entry Point
+ * @args: input
+ * @env: environmental variable
+ * Return: 0 Always
+ */
+int builtin_cmd(char **args, char **env)
+{
+	if (strcmp(args[0], "exit") == 0)
+	{
+		_printst("\n");
+		free_args(args);
+		exit(0);
+	}
+	else if (strcmp(args[0], "cd") == 0)
+	{
+		if (args[1] == NULL)
+		{
+			chdir(_getenv("HOME"));
+		}
+		else
+		{
+			if (chdir(args[1]) != 0)
+			{
+				perror("Error: invalid command");
+			}
+		}
+		free_args(args);
+		return (1);
+	}
+	if (strcmp(args[0], "env") == 0)
+	{
+		_env(env);
+		free_args(args);
+		return (1);
+	}
+	return (0);
+}
+/**
+ * _env - Entry point
+ * @env: environment variable
+ */
+void _env(char **env)
+{
+	int i = 0;
+
+	while (env[i])
+	{
+		_printst(env[i]);
+		_printst("\n");
+		i++;
+	}
+}
